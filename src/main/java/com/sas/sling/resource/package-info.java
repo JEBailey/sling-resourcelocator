733374825d944563b7c83e0435c91b7da394c9c9
JE Bailey <jason.bailey@sas.com>
2017-10-09 08:57:40 -0400

    clarifying api change in version numbers

--- src/main/java/com/sas/sling/resource/package-info.java
+++ src/main/java/com/sas/sling/resource/package-info.java
@@ -11,5 +11,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@org.osgi.annotation.versioning.Version("0.1.9")
+@org.osgi.annotation.versioning.Version("1.0.0")
 package com.sas.sling.resource;
